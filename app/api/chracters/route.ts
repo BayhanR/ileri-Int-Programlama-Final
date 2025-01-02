56067865fcb112c1cb3003a1e3b856f09a97fb08
BayhanR <91837307+BayhanR@users.noreply.github.com>
2025-01-03 01:42:52 +0300

    Update route.ts

--- app/api/chracters/route.ts
+++ app/api/chracters/route.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 export async function GET(request: Request) {
-    const res = await axios.get("https://api.themoviedb.org/3/tv/34587/credits?api_key=3b3b9c816ffa9292cbfb27de2b1ffe58")
+    const res = await axios.get("https://api.themoviedb.org/3/tv/34587/credits?api_key=YOUR_API_KEY")
   
     return Response.json({
       cast: res.data.cast,
